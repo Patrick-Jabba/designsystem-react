3f580543ca3048a5f7f22b06ddece32e9e1ffc87
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-10-11 22:04:06 -0300

    projeto inicial com vite react typescript - instalação do tailwindcss e postcss escolha da fonte global e instalação do storybook builder

---
+++ lab-ds/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  );
+}
