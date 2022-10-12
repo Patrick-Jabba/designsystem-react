edbd4b34b12b744615e87b87ffe72f284aca31ee
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-10-12 19:24:55 -0300

    ci: Add publish storybook workflow

--- lab-ds/.storybook/main.cjs
+++ lab-ds/.storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  vitefinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION'){
+      config.base = '/ignite-lab-design-system/'
+    }
+
+    return config;
   }
 }
